2436d8bd699c3b6f8256a158413d4e86b72a6726
chandran4725 <chandran.g.work@gmail.com>
2026-08-17 11:45:34 +0530

    Building for production

--- app/main.py
+++ app/main.py
@@ -23,6 +23,7 @@ origins = [
     "http://localhost:5173",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:5173",
+    "https://issue-tracker-fyobo6j7k-chandrans-projects-df24d617.vercel.app"
 ]
 if settings.FRONTEND_URL and settings.FRONTEND_URL not in origins:
     origins.append(settings.FRONTEND_URL)
