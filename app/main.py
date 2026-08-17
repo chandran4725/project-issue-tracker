ac508e4845b5eaf9c193ff14f5b839ee3883bc82
chandran4725 <chandran.g.work@gmail.com>
2026-08-17 12:36:17 +0530

    cors fixed deployement

--- app/main.py
+++ app/main.py
@@ -20,10 +20,6 @@ app = FastAPI(redirect_slashes=False)
 
 origins = [
     "https://issue-tracker-frontend-snowy.vercel.app",
-    "http://localhost:3000",
-    "http://localhost:5173",
-    "http://127.0.0.1:3000",
-    "http://127.0.0.1:5173",
 ]
 
 app.add_middleware(
